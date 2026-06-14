ee1b982ba2bf50894c508ef534a9e107cf762393
William de Araujo Almeida <estudoswill@gmail.com>
2026-06-14 19:28:02 -0300

    fix: allow cross-origin requests from custom domain in mlflow

--- fase_2/infra/mlflow/main.tf
+++ fase_2/infra/mlflow/main.tf
@@ -249,7 +249,8 @@ resource "aws_instance" "mlflow_server" {
       --host 0.0.0.0 \
       --backend-store-uri "$DB_URI" \
       --default-artifact-root "s3://${aws_s3_bucket.mlflow_artifacts.bucket}" \
-      --allowed-hosts "*"
+      --allowed-hosts "*" \
+      --cors-allowed-origins "*"
   EOF
 }
 
